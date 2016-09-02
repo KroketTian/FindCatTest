c40be503e215fd8adf1b38de3c1f5d5b95691076
KroketTian <347913886@qq.com>
2016-09-02 17:55:10 +0800

    doing...

--- map/js/marker.js
+++ map/js/marker.js
@@ -1,9 +1,25 @@
 /**
  * Created by KrometTian on 2016/9/2.
  */
-function marker(){
-    this.marketType = '';
+function marker(option){
+    var me = this;
+    this.markerType = option.markerType;
+    this.lng = 0;
+    this.lat = 0;
+    this.content = "";
+    this.icon = null;
+
+    this.setmarkerType = function(markerType){
+        me.margkerType = markerType;
+    };
+    this.getmarkerType = function(){
+        return me.margkerType;
+    };
     this.create = function(option){
-        
-    }
+        var marker = new AMap.Marker({
+            position:[lng,lat],
+            content:me.content,
+            icon:me.icon
+        })
+    };
 }
