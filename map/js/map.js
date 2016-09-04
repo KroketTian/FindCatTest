77423f42d56dcd6210d69cffb5de02694d66c8a6
KroketTin <krokettin@foxmail.com>
2016-09-05 00:17:48 +0800

    doing...

--- map/js/map.js
+++ map/js/map.js
@@ -2,10 +2,46 @@
  * Created by kroketTian on 2016/9/2.
  */
 function map(){
+    this.map = null;
+
+    var me = this;
+
     this.createmap = function(container){
-        this.map = new AMap.Map(container,{
+        me.map = new AMap.Map(container,{
             resizeENable: true,
             zoom:10
+        })
+    }
+    this.addMarkerByajax = function(url,data){
+        $.ajax({
+            url:url,
+            data:data,
+            type: "POST",
+            dataType: "JSON",
+            async: false,
+            success:function(data){
+                console.log(data.data);
+                if(data.data){
+                    if(data.type === "catmarker") {
+                        for (var i = 0; i < data.data.length; i++) {
+                            var marker = new catmarker({
+                                markerType: data.data[i].type,
+                                lng: data.data[i].LONGITUDE,
+                                lat: data.data[i].LATITUDE,
+                                content: {
+                                    imgurl: data.data[i].PICURL,
+                                    catname: data.data[i].NAME,
+                                    cathost: data.data[i].cathost
+                                }
+                            });
+                            marker.create();
+                            marker.setIntoMap(me.map);
+                            me.map.setCenter(marker.mapmarker.getPosition());
+                        }
+                    }
+                    addCluster(1,me.map,markers);
+                }
+            }
         })
     }
 }
--- map/js/marker.js
+++ map/js/marker.js
@@ -1,25 +1,61 @@
 /**
- * Created by KrometTian on 2016/9/2.
+ * Created by KroketTian on 2016/9/2.
  */
-function marker(option){
+
+var cluster;
+var markers = [];
+var catMarkers = [];
+function catmarker(option){
     var me = this;
     this.markerType = option.markerType;
-    this.lng = 0;
-    this.lat = 0;
-    this.content = "";
-    this.icon = null;
+    this.lng = option.lng;
+    this.lat = option.lat;
+    this.content = "<div class='catmarker'>" +
+        "<img src='" + option.content.imgurl +"'alt='猫图片'/>" +
+        "<div class =  'catmarkerP'>" +
+        "<p>猫猫：" + option.content.catname + "</p>" +
+        "<p>主人：" + option.content.cathost + "</p>" +
+        "</div>";
+    // this.icon = null;
+    this.mapmarker = null;
 
-    this.setmarkerType = function(markerType){
-        me.margkerType = markerType;
-    };
-    this.getmarkerType = function(){
-        return me.margkerType;
-    };
-    this.create = function(option){
-        var marker = new AMap.Marker({
-            position:[lng,lat],
+    this.create = function(){
+        me.mapmarker = new AMap.Marker({
+            position:[me.lng,me.lat],
             content:me.content,
-            icon:me.icon
+            // icon:me.icon
         })
+        markers.push(me.mapmarker);
+        catMarkers.push(me.mapmarker);
+
+        // me.mapmarker.setMap(mapObj);
     };
+    this.setIntoMap = function(mapObj){
+        if(me.mapmarker){
+            me.mapmarker.setMap(mapObj);
+        }
+    };
+}
+// 添加点聚合
+function addCluster(tag,mapObj,markers) {
+    if (cluster) {
+        cluster.setMap(null);
+    }
+    if (tag == 1) {
+        var sts = [{
+            url: "",
+            size: new AMap.Size(32, 32),
+            offset: new AMap.Pixel(-16, -30),
+            // textColor:'transparent'
+        }];
+        mapObj.plugin(["AMap.MarkerClusterer"], function() {
+            cluster = new AMap.MarkerClusterer(mapObj, markers, {
+                styles: sts
+            });
+        });
+    } else {
+        map.plugin(["AMap.MarkerClusterer"], function() {
+            cluster = new AMap.MarkerClusterer(map, markers);
+        });
+    }
 }
